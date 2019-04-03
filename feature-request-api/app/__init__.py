a308619c8949c164db0eed00eeb6d51d679aefac
Ivange Larry <ivangelarry@gmail.com>
2019-04-03 23:53:18 +0100

    initial commit

---
+++ feature-request-api/app/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRETE_KEY='dev',
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.instance_path, 'app.db'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World'
+
+    return app
