070ea0863f1624d35b416b468565bb4100fda143
Ivange Larry <ivangelarry@gmail.com>
2019-04-04 20:08:18 +0100

    Add delete functionality in web app

--- web-app/js/app.js
+++ web-app/js/app.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
     var tableData = []
-    $.get('http://127.0.0.1:5000/api/tickets', function (data, status) {
-        tableData = data['data'];
-        console.log(JSON.stringify(tableData))
-    });
 
-    $('#myTable').DataTable({
+    var table = $('#myTable').DataTable({
         'processing': true,
         'ajax': {
             'url': 'http://127.0.0.1:5000/api/tickets',
         },
         'columns': [
-            {"data": "id"},
             {"data": "title"},
             {"data": "description"},
             {"data": "client"},
             {"data": "product_area"},
             {"data": "priority"},
-            {"data": "target_date"}
+            {"data": "target_date"},
+            {
+                mRender: function(data, type, row) {
+                    return '<a href="#"><span class="glyphicon glyphicon-pencil" data-id="' + row.id + '" class="edit-row"></span></a> / <a href="#"><span class="glyphicon glyphicon-remove" data-id="' + row.id + '" class="delete-row"></span></a>'
+                }
+            },
         ]
     });
+
+    $('#myTable').on('click', 'span', function(){
+        var shouldDelete = confirm('Are you sure you want to delete this record?')
+        if (shouldDelete) {
+            var id = $(this).attr('data-id');
+            $.ajax({
+                url: 'http://127.0.0.1:5000/api/tickets/' + id,
+                type: 'DELETE',
+                success: function(result) {
+                    table.ajax.reload(null, false);
+                }
+            })
+        }
+
+    })
 });
